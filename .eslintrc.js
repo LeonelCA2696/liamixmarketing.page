2d36941cde06241a14c046bc0ba69eec342a305e
LeonelCA2696 <leonelca2696@gmail.com>
2023-11-13 17:22:28 -0400

    Modificacion de configuracion

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,8 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: [
-    "eslint:recomended",
-    "plugin:jsx-a11y/recomended",
-    "plugin:prettier/recomended",
-    "next",
-    "next/core-web-vitals",
-  ],
+  extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
   rules: {
-    semi: ["error", "always"],
+    semi: ['error', 'always'],
   },
 };
